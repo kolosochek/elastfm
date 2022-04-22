38dfbaf7de9e398ebca51e3e73cccb5e15c1e33f
D <kolosochek@gmail.com>
2022-04-23 00:06:21 +0300

    changed django version

--- lastfm_downloader/settings.py
+++ lastfm_downloader/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_ROOT = "/data/lastfm_downloader/lastfm_downloader/core/static/"
+#STATIC_ROOT = "/data/lastfm_downloader/lastfm_downloader/core/static/"
 STATIC_URL = 'static/'
 #STATICFILES_DIRS = [
 #    BASE_DIR / "static",
