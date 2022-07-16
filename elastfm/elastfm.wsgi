cd97d618fa24e27d6ce36940c26f0408d17b84b9
D <kolosochek@gmail.com>
2022-07-16 15:55:57 +0300

    minor refactor

--- elastfm/elastfm.wsgi
+++ elastfm/elastfm.wsgi
@@ -1,5 +1,6 @@
 import os
 import django
+import elastfm.settings
 
 from django.core.wsgi import get_wsgi_application
 
--- elastfm/wsgi.py
+++ elastfm/wsgi.py
@@ -1,13 +1,5 @@
-"""
-WSGI config for lastfm_downloader project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
-"""
-
 import os
+import elastfm.settings
 
 from django.core.wsgi import get_wsgi_application
 
